95d3cb87fb2a03adf71774ad5d0830a2a38dd141
BaconBonBon <ken900522@gmail.com>
2026-09-16 00:40:02 +0800

    Improve lyric navigation, section headings, wrapped lines and mobile controls

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'romaji-v2-20260918-svg-settings-3';
+const CACHE = 'romaji-v2-20260918-navigation-4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
