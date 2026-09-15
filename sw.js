41d43315a9a0cdeb3d3268630064190d880dc6b9
BaconBonBon <ken900522@gmail.com>
2026-09-16 00:28:20 +0800

    Replace both settings glyphs with consistent SVG line icons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'romaji-v2-20260918-reading-2';
+const CACHE = 'romaji-v2-20260918-svg-settings-3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
