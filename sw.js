5a39eebdc8a5ecb02a1546a1b05ffcd6ca209c1f
BaconBonBon <ken900522@gmail.com>
2026-09-15 23:59:41 +0800

    Add subtle interactions, clearer sing-along mode and persistent theme controls

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'romaji-v2-20260915';
+const CACHE = 'romaji-v2-20260918-interactions';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
