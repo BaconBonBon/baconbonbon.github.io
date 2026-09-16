7229509d2ef363afddd874b7e284bb88fe5ca152
BaconBonBon <ken900522@gmail.com>
2026-09-16 22:19:29 +0800

    Streamline JOYSOUND verification return flow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'romaji-v2-20260916-joysound-8';
+const CACHE = 'romaji-v2-20260916-joysound-return-9';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
