b604f5f4ecdf160be17df519fb302c74b3bb3dfb
BaconBonBon <ken900522@gmail.com>
2026-09-16 18:06:27 +0800

    Improve lyric workflow with section navigation, persistent progress and song preferences

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'romaji-v2-20260916-search-5';
+const CACHE = 'romaji-v2-20260916-workflow-6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
