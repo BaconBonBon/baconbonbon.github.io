c09bcb7a877f0092c286daea1715f52043a05d5b
BaconBonBon <ken900522@gmail.com>
2026-09-16 22:16:04 +0800

    Add JOYSOUND search, availability status and song numbers

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'romaji-v2-20260916-search-align-7';
+const CACHE = 'romaji-v2-20260916-joysound-8';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
