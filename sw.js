ff3aa7434c3f5b810a32678017295804cbc97c11
BaconBonBon <ken900522@gmail.com>
2026-09-24 11:03:57 +0800

    Compact search actions and add lyrics return button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'romaji-v2-20260924-favorites-number-13';
+const CACHE = 'romaji-v2-20260924-compact-search-top-14';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
