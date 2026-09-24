87510c897b89268987c1cc22016248e1771a20ac
BaconBonBon <ken900522@gmail.com>
2026-09-24 12:48:56 +0800

    Refresh PWA cache for lyrics audit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'romaji-v2-20260924-home-guide-18';
+const CACHE = 'romaji-v2-20260924-lyrics-audit-19';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', event => {
